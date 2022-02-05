8855a162bb319f51f409e4126d9014af374da882
Mehmet ARICI <mehmet.arici@orema.com.tr>
2022-02-05 19:51:21 +0300

    project layout, navbar and sample dashboard page implemented

---
+++ vue/src/components/index.js
@@ -0,0 +1,2 @@
+export {default as DefaultLayout} from "./layout/DefaultLayout.vue"
+export {default as PageContainer} from "./layout/PageContainer.vue"
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,16 @@
 import {createRouter, createWebHistory} from "vue-router";
+import {DefaultLayout} from "../components";
+import {Dashboard} from "../views";
 
-const routes = [{}]
+const routes = [{
+    path: "/",
+    redirect: "/dashboard",
+    component: DefaultLayout,
+    meta: {requiresAuth: true},
+    children: [
+        {path: "/dashboard", name: "Dashboard", component: Dashboard},
+    ]
+}]
 
 const router = createRouter({
     history: createWebHistory(), routes
