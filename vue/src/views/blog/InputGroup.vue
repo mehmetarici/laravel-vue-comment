5956756b0823de98fe07c7d1e65eea05a13eea7b
Mehmet ARICI <mehmet.arici@orema.com.tr>
2022-02-06 02:29:06 +0300

    endpoint integration and vuex implemented

--- vue/src/views/blog/InputGroup.vue
+++ vue/src/views/blog/InputGroup.vue
@@ -2,24 +2,30 @@
     <div class="flex ">
         <Avatar :avatarImg="avatarImg"/>
         <div class="pl-2 w-full relative rounded-md">
-            <Input placeholder="Type your comment here..."/>
+            <Input :value="input.value" :placeholder="input.placeholder"/>
             <div class="absolute inset-y-0 right-0 flex items-center">
                 <button
+                    @click="button.click"
                     class="cursor-pointer h-full py-0 pl-2 pr-4 bg-transparent text-sky-600 sm:text-sm font-bold">
-                    {{buttonTitle}}
+                    {{ button.title }}
                 </button>
             </div>
         </div>
     </div>
-
+    <h1>{{input.value}}</h1>
 </template>
 
 <script setup>
 import {Input, Avatar} from "../index";
-
 defineProps({
-    placeholder: String,
-    buttonTitle: String,
+    input: {
+        placeholder: String,
+        value: String,
+    },
+    button: {
+        title: String,
+        click: Function
+    },
     avatarImg: String
 })
 </script>
