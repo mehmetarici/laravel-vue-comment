98c292006dbff845841d321438928d9ad82e594b
Mehmet ARICI <mehmet.arici@orema.com.tr>
2022-02-06 10:46:46 +0300

    comment tree components renamed

--- vue/src/views/index.js
+++ vue/src/views/index.js
@@ -2,6 +2,6 @@ export {default as Dashboard} from "./Dashboard.vue";
 export {default as PostDetail} from "./PostDetail.vue";
 
 export {default as Blog} from "./blog/Blog.vue";
-export {default as CommentContent} from "./blog/CommentContent.vue";
-export {default as CommentTreeNode} from "./blog/CommentTreeNode.vue";
+export {default as CommentNode} from "./blog/CommentNode.vue";
+export {default as CommentTree} from "./blog/CommentTree.vue";
 export {default as InputGroup} from "./blog/InputGroup.vue";
