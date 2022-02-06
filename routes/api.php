abd14b7435530b1bb9898802364d38712ff9e88b
Mehmet ARICI <mehmet.arici@orema.com.tr>
2022-02-06 11:04:10 +0300

    API endpoint documentations

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,6 @@
 use App\Http\Controllers\PostCommentController;
 use Illuminate\Support\Facades\Route;
 
-// TODO not required for now
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
 // TODO postId for testing to complete API routing mechanism
 Route::prefix('posts/{postId}')->group(function () {
     Route::get('/comments', [PostCommentController::class, "listComments"])->name("list-comments");
